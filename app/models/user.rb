041e2ec7983b7f5bd84646c21f863e5e974d0e1a
mikeygbd <mikey.soares@gmail.com>
2019-06-21 14:03:41 -0400

    added has many to user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ApplicationRecord
   has_secure_password
-validates :email, uniqueness: {case_sensitive: false}, presence: true
-validates :firstname, :lastname, presence: true
+validates :email, presence: true
+# validates :firstname, :lastname, presence: true
 
 has_many :wishlist_games
 has_many :games, through: :wishlist_games
-
+has_many :secrets
 end
