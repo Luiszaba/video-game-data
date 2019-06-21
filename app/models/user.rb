117ae94c9c758e91fe7ceccae9fa699995b12f85
mikeygbd <mikey.soares@gmail.com>
2019-06-20 22:41:12 -0400

    deleted and remade users table

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ApplicationRecord
   has_secure_password
-  validates :email, uniqueness: {case_sensitive: false}, presence: true
-  validates :firstname, :lastname, presence: true
+validates :email, uniqueness: {case_sensitive: false}, presence: true
+validates :firstname, :lastname, presence: true
 
-  has_many :wishlist_games
-  has_many :games, through: :wishlist_games
+has_many :wishlist_games
+has_many :games, through: :wishlist_games
+
 end
