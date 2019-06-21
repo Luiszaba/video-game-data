b3a8f040c675e88220510d517c04cd7fcd0ccd23
mikeygbd <mikey.soares@gmail.com>
2019-06-21 14:07:26 -0400

    created seed file

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2019_06_21_022652) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "games", force: :cascade do |t|
     t.string "name"
     t.integer "rating"
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+  mike = User.create(firstname: "Mike", lastname: "Soares", email: "mikey.soares@gmail.com", password: "password")
+  ryan = User.create(firstname: "Ryan", lastname: "Reece", email: "joe@gmail.com", password: "password")
+  chris = User.create(firstname: "Chris", lastname: "Reece", email: "chris@gmail.com", password: "password")
+
+  mike.secrets.create(content: "I secretly want to finish this course.")
+  ryan.secrets.create(content: "I love sniffing farts.")
+  chris.secrets.create(content: "I love cheese!")
+  mike.secrets.create(content: "I need coffee!")
+  chris.secrets.create(content: "My favorite color is pink.")
+  ryan.secrets.create(content: "I like to eat cake.")
