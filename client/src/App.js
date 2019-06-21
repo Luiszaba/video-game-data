2bb0999c1d4981ecd5ad3ea9d2e31dabaf48ecff
mikeygbd <mikey.soares@gmail.com>
2019-06-21 14:04:59 -0400

    created test app.js to try and get auth working

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,124 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Login from './components/Login'
+import Secrets from './components/Secrets'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+
+  constructor() {
+    super()
+  this.state = {
+    currentUser: null,
+    loginForm: {
+      email: "",
+      password: ""
+    },
+      secrets: []
+  }
+}
+
+componentDidMount() {
+  const token = localStorage.getItem("token")
+  if (token) {
+  fetch("http://localhost:3001/get_current_user", {
+    headers: {
+      "Authorization": token
+    }
+  })
+  .then(r => r.json())
+  .then(resp => {
+    if (resp.error) {
+      alert(resp.error)
+    } else {
+      this.setState({
+        currentUser: resp.user
+      })
+    }
+  })
+  
+  }
+}
+
+
+
+  handleLoginFormChange = event => {
+    const { name, value } = event.target
+    this.setState({
+      loginForm: {
+      ...this.state.loginForm,
+      [name]: value
+      }
+    })
+  }
+
+  handleLoginFormSubmit = event => {
+    event.preventDefault()
+    const userInfo = this.state.loginForm
+    const headers = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+      user: userInfo
+      })
+    }
+
+    fetch("http://localhost:3001/login", headers)
+    .then(r => r.json())
+    .then(userJSON => {
+      if (userJSON.error) {
+        // failure
+        alert("Invalid Credentials")
+      } else {
+        //success
+        this.setState({
+          currentUser: userJSON
+        })
+      }
+    })
+    .catch(console.log)
+
+  }
+
+  getSecrets = () => {
+    const token = localStorage.getItem("token")
+    fetch('http://localhost:3001/secrets', {
+      headers: {
+        "Authorization": token
+      }
+    })
+    .then(r => r.json())
+    .then(secrets => {
+      if (secrets.error) {
+        alert("Not authorized for secrets.")
+      } else {
+        this.setState({
+          secrets
+        })
+      }
+    })
+    .catch(console.log)
+  }
+
+  render() {
+    const { currentUser } = this.state
+    return (
+      <div className="App">
+        <h2>{currentUser ? `Logged in as ${currentUser.firstname} ${currentUser.lastname}` : "Not logged in."}</h2>
+
+      <Login
+          handleLoginFormChange={this.handleLoginFormChange}
+          email={this.state.loginForm.email}
+          password={this.state.loginForm.password}
+          handleLoginFormSubmit={this.handleLoginFormSubmit}
+           />
+         <button onClick={this.getSecrets}>Show User's Secrets</button>
+         <Secrets secrets={this.state.secrets} />
+      </div>
+    );
+  }
 }
 
 export default App;
