2aebd9c846f3dc4ae4632de5e82f782a15739864
mikeygbd <mikey.soares@gmail.com>
2019-06-21 21:30:16 -0400

    created a creatAction helper

--- client/src/helpers/redux.helpers.js
+++ client/src/helpers/redux.helpers.js
@@ -1,6 +1,15 @@
+
+
 export const createReducer = (initialState = {}, actionHandlerKeyFuncs = {}) => {
   return (state = initialState, action) => {
     const actionHandler = actionHandlerKeyFuncs[action.type]
     return actionHandler ? actionHandler(state, action) : state
+  }
+}
+
+const createAction = (type, actionProps) => {
+  return {
+    type,
+    ...actionProps
   }
 }
